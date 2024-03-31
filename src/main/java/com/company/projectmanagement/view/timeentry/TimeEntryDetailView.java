b6ce53f61b57736717c43a722301f696a4b46e86
Alexandra <s7125@mail.ru>
2024-03-31 20:27:21 +0500

    autocomlete field user in TimeEntry

--- src/main/java/com/company/projectmanagement/view/timeentry/TimeEntryDetailView.java
+++ src/main/java/com/company/projectmanagement/view/timeentry/TimeEntryDetailView.java
@@ -1,9 +1,14 @@
 package com.company.projectmanagement.view.timeentry;
 
 import com.company.projectmanagement.entity.TimeEntry;
+import com.company.projectmanagement.entity.User;
 import com.company.projectmanagement.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.TimeSource;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.time.LocalDateTime;
 
 @Route(value = "timeEntries/:id", layout = MainView.class)
@@ -11,8 +16,15 @@ import java.time.LocalDateTime;
 @ViewDescriptor("time-entry-detail-view.xml")
 @EditedEntityContainer("timeEntryDc")
 public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
+    @Autowired
+    private TimeSource timeSource;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
     @Subscribe
     public void onInitEntity(final InitEntityEvent<TimeEntry> event) {
-        event.getEntity().setEntryDate(LocalDateTime.now());
+        event.getEntity().setEntryDate(timeSource.now().toLocalDateTime());
+        final User user = (User) currentAuthentication.getUser();
+        event.getEntity().setUser(user);
     }
 }
